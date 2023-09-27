db6e4e4c46f46f021cc9bfe138dada0cf142151d
Hapaxia <Hapaxia@users.noreply.github.com>
2023-09-27 20:59:00 +0100

    patch ParseToLingo
    quick patch to "fix" ParseToLingo as it creates a language for each language it parses but Lingo starts with one already so there is an unused language.
    
    Note that this parser uses the first languages so it can't add languages to an existing set of languages; it's presumed to be an empty Lingo object.

--- Parsers/ParseToLingo.cpp
+++ Parsers/ParseToLingo.cpp
@@ -67,6 +67,7 @@ bool parseToLingo(const std::string& filename, Lingo<T>& lingo)
 
 	const std::size_t numberOfLanguages{ result[0u].size() };
 	for (std::size_t l{ 0u }; l < numberOfLanguages; ++l)
+	for (std::size_t l{ 1u }; l < numberOfLanguages; ++l)
 		lingo.addLanguage();
 	for (std::size_t l{ 0u }; l < numberOfLanguages; ++l)
 	{
